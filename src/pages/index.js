17542f15e2365742d8089c5cb51a1dd05cd93f5f
Bright <barhin49@gmail.com>
2020-04-29 19:08:09 +0000

    Gatsby Blog final

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
-import React from "react"
+import React from 'react'
+import Layout from '../components/layout/layout.component'
+import Head from '../components/head'
 
-export default () => <div>Hello world!</div>
+const IndexPage = ()=>{
+    return(
+        <Layout>
+            <Head title='Home'/>
+            <h2>Hello</h2>
+            <p>Gatsby for starters</p>
+        </Layout>
+    )
+}
+
+export default IndexPage
